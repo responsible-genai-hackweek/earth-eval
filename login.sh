79b9359f0e3a3ee1c84e05addd34cabe2d4dc2d4
Keiko Nomura <45358635+nkeikon@users.noreply.github.com>
2026-08-27 21:43:48 -0700

    Delete login.sh

--- login.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-read -rsp "Paste your Earthdata token: " EARTHDATA_TOKEN
-echo
-export EARTHDATA_TOKEN
-python3 -c "
-import earthaccess
-auth = earthaccess.login(strategy='environment', persist=False)
-print('Authenticated:', auth.authenticated)
-"
